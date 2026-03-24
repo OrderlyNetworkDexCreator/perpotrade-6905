6bfd7b2ff346a33ec36bf81af43f1c86ee762e8c
Mario Reder <mario.reder@pm.me>
2026-03-24 07:05:34 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets,Rewards,Vaults,Leaderboard,Points",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "API Docs, https://docs.perpo.trade/build-on-omnichain/introduction",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
