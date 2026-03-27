157d47dffe32abc34da97dce9e0e557b0000efa5
Mario Reder <mario.reder@pm.me>
2026-03-27 21:10:33 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Perpo.trade is a next-generation decentralized exchange (DEX) for perpetual futures trading, offering low fees, deep liquidity, and seamless on-chain execution.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets,Rewards,Vaults,Leaderboard,Points",
+  "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets,Rewards,Vaults,Points",
   "VITE_CUSTOM_MENUS": "API Docs, https://docs.perpo.trade/build-on-omnichain/introduction",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
