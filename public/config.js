d8b1d2c8f1cd67338af538a85910f0329845eca8
orderly-guoqing <guoqing@orderly.network>
2026-08-31 21:03:56 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -20,7 +20,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets,Rewards,Vaults,Points",
-  "VITE_CUSTOM_MENUS": "API Docs, https://docs.zild.finance/build-on-omnichain/introduction",
+  "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
